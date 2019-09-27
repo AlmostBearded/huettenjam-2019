fd3e65291b1c82de17de121e3489281639cb3ae8
enrlor <enricaloria94@gmail.com>
2019-09-27 21:02:21 +0200

    update csv parser

--- scripts/pool.gd
+++ scripts/pool.gd
@@ -17,33 +17,49 @@ func pull(id):
 		return null
 	var card = dict[id]
 	dict.erase(id)
+	size = size - 1
 	return card
 
 func select_succ(succ_list):
+	if(size == 0):
+		return -1
 	randomize()
 	#if no successors get one of the general ones at random
 	if succ_list.empty():
-		var random = randi() % 100
-		var possible_succ = []
-		for key in dict.keys():
-			if dict[key].is_general():
-				possible_succ.append(key)
-		return possible_succ[randi() % possible_succ.size()]
+		return get_general_card()
 	#just one successors
 	elif succ_list.size() == 1:
 		return succ_list.keys()[0]
 	#mor than one successors
 	else: 
-		var random = randi() % 100
-		var range_start = 0
+		#Select the succ that are still in the pool of cards
+		var actual_succ_list = {}
+		var option_sum = 0
 		for key in succ_list.keys():
-			var range_end = succ_list[key]
-			if random >= range_start and random <= int(range_end):
-				return key
-			else:
-				range_start = int(range_end)
+			if(dict.has(key)):
+				option_sum += int(succ_list[key])
+				actual_succ_list[key] = succ_list[key]
+		#if there are no succecsors take one of the general 
+		if(option_sum == 0):
+			return get_general_card()
+		else:
+			var random = randi() % option_sum
+			var range_start = 0
+			
+			for key in succ_list.keys():
+				var range_end = int(succ_list[key]) + range_start
+				if random >= range_start and random <= range_end:
+					return key
+				else:
+					range_start = range_end
 		
 
-	
+func get_general_card():
+	var random = randi() % 100
+	var possible_succ = []
+	for key in dict.keys():
+		if dict[key].is_general():
+			possible_succ.append(key)
+	return possible_succ[randi() % possible_succ.size()]		
 
 
