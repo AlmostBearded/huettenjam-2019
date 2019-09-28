c46dd200f6b1166489a3bceb583b89f34919e317
enrlor <enricaloria94@gmail.com>
2019-09-28 16:17:32 +0200

    integrated data

--- scripts/cards_pool.gd
+++ scripts/cards_pool.gd
@@ -6,16 +6,18 @@ var index_offset = 6
 var n_c_stats = 4
 var n_d_stats = 3
 var n_dep = 3
-var n_stats =  n_c_stats + (n_d_stats * n_dep)
+var n_all_stats =  (n_c_stats + n_d_stats * n_dep) * 2
 
+var pos_indexes = [0,1,2,3,8,9,10,14,15,16,20,21,22]
+var neg_indexes = [4,5,6,7,11,12,13,17,18,19,23,24,25]
 
 func load_csv():
 	var file = File.new()
-	file.open("res://data/cards_pool.csv", file.READ)
+	file.open("res://data/story_data.csv", file.READ)
 	file.get_csv_line ()
 	var header = file.get_csv_line ()
 	var stats = []
-	for i in range(index_offset, index_offset+2*n_stats):
+	for i in range(index_offset, index_offset+n_all_stats):
 		stats.append(header[i])
 	while !file.eof_reached():
 		var csv = file.get_csv_line()
@@ -45,25 +47,26 @@ func build_card(line, stats):
 	#index 19-31
 	var neg_e = {}
 	
-	for stat in range(0,n_stats):
-		pos_e[stats[stat]] = line[index_offset+stat]
+	for stat in pos_indexes:
+		var name = stats[stat].replace("pos_","")
+		pos_e[name] = line[index_offset+stat]
 		
-	for stat in range(n_stats, 2*n_stats):
-		neg_e[stats[stat]] = line[index_offset+stat]
+	for stat in neg_indexes:
+		var name = stats[stat].replace("neg_","")
+		neg_e[name] = line[index_offset+stat]
 	
-	#index 32
+	
 	var pos_s = {}
-	var succ = line[index_offset+2*n_stats]
+	var succ = line[index_offset + 26]
 	if(succ.length() > 2):
 		succ = succ.substr(1,succ.length()-2)
 		for s in succ.rsplit(','):
 			var item = s.rsplit(':')
 			pos_s[item[0]] = item[1]
 	
-	#index 33
 	var neg_s = {}
+	succ = line[index_offset + 27]
 	if(succ.length() > 2):
-		succ = line[index_offset+2*n_stats + 1]
 		succ = succ.substr(1,succ.length()-2)
 		for s in succ.rsplit(','):
 			var item = s.rsplit(':')
--- scripts/department_instance.gd
+++ scripts/department_instance.gd
@@ -39,7 +39,7 @@ func initiate_department():
 		custom_stats.append(stats[i])
 		local_i += 1
 		
-	text.text = str(curr_card.id) + ". " + curr_card.task
+	text.text = "DILEMMA: " + curr_card.task
 	
 	choice == NEUTRAL
 	update_stats_ui(get_all_stats())
@@ -56,11 +56,11 @@ func get_input():
 			comupute_influences(curr_card, true)
 			go_ahead(true)
 			choice = NEUTRAL
-			update_stats_ui(get_all_stats())
+			reset_indicators_ui()
 		else:
 			text.text = str(curr_card.id) + ". " + curr_card.task
 			choice = NEUTRAL	
-			update_stats_ui(get_all_stats())
+			reset_indicators_ui()
 	if Input.is_action_just_pressed("reject_p%s" %id):
 		if choice == NEUTRAL:
 			simulate_influences(curr_card, false)
@@ -70,11 +70,11 @@ func get_input():
 			comupute_influences(curr_card, false)
 			go_ahead(false)
 			choice = NEUTRAL
-			update_stats_ui(get_all_stats())
+			reset_indicators_ui()
 		else:
 			text.text = str(curr_card.id) + ". " + curr_card.task
 			choice = NEUTRAL	
-			update_stats_ui(get_all_stats())
+			reset_indicators_ui()
 			
 			
 func simulate_influences(curr_card, answer):
@@ -86,7 +86,7 @@ func simulate_influences(curr_card, answer):
 	for i in range(custom_stats_start_index, custom_stats_start_index + GlobalPools.n_d_stats):
 		l.append(effects.values()[i])	
 	
-	update_stats_ui(simulate_update(l))
+	update_indicators_ui(l)
 	
 		
 func comupute_influences(curr_card, answer):
@@ -103,19 +103,24 @@ func comupute_influences(curr_card, answer):
 			l.append(effects.values()[i])
 		dep_deltas[str(dep)] = l
 	
-	var gov = effects['rev'] 
+	var gov = effects['gov'] 
 	var env = effects['env']  
-	var cust = effects['cost'] 
-	var stak = effects['stak'] 
+	var cust = effects['custom'] 
+	var stak = effects['stake'] 
 	Company.update_stats(gov, env, cust, stak, dep_deltas)
-	
-	#Bad code to get the stats - simulate increase with delta 0
 	update_stats_ui(get_all_stats())
 	
 func update_stats_ui(values):
 	for i in range(0, GlobalPools.n_d_stats):
-		my_status[i].show_influence(values[i])
+		my_status[i].set_influence(float(values[i]))
 
+func update_indicators_ui(values):
+	for i in range(0, GlobalPools.n_d_stats):
+		my_status[i].show_influence(float(values[i]))	
+		
+func reset_indicators_ui():
+	for i in range(0, GlobalPools.n_d_stats):
+		my_status[i].reset_influence()
 		
 func go_ahead(answer):
 	var succ_id = pool.select_succ(curr_card, true)
@@ -123,52 +128,7 @@ func go_ahead(answer):
 	if(curr_card.is_general()):
 		pool.insert(curr_card)
 	curr_card = new_card
-	text.text = str(curr_card.id) + ". " + curr_card.task		
+	text.text = "DILEMMA: " + curr_card.task		
 	
 	
 	
-		
-#OLD STUFF		
-	
-func show_left_influences(curr_card):
-	var name = 'neg_%s'%$Status/Label.text
-	$Status.show_influence(-int(curr_card.negative_effects[name]))
-	name = 'neg_%s'%$Status2/Label.text
-	$Status2.show_influence(-int(curr_card.negative_effects[name]))
-	name = 'neg_%s'%$Status3/Label.text
-	$Status3.show_influence(-int(curr_card.negative_effects[name]))
-
-func show_right_influences(curr_card):
-	var name = 'pos_%s'%$Status/Label.text
-	$Status.show_influence(int(curr_card.positive_effects[name]))
-	name = 'pos_%s'%$Status2/Label.text
-	$Status2.show_influence(int(curr_card.positive_effects[name]))
-	name = 'pos_%s'%$Status3/Label.text
-	$Status3.show_influence(int(curr_card.positive_effects[name]))
-	
-func set_left_influences(curr_card):
-	var name = 'neg_%s'%$Status/Label.text
-	$Status.set_influence(-int(curr_card.negative_effects[name]))
-	name = 'neg_%s'%$Status2/Label.text
-	$Status2.set_influence(-int(curr_card.negative_effects[name]))
-	name = 'neg_%s'%$Status3/Label.text
-	$Status3.set_influence(-int(curr_card.negative_effects[name]))
-
-func set_right_influences(curr_card):
-	var name = 'pos_%s'%$Status/Label.text
-	$Status.set_influence(int(curr_card.positive_effects[name]))
-	name = 'pos_%s'%$Status2/Label.text
-	$Status2.set_influence(int(curr_card.positive_effects[name]))
-	name = 'pos_%s'%$Status3/Label.text
-	$Status3.set_influence(int(curr_card.positive_effects[name]))
-	
-func reset_influences():
-	for s in my_status:
-		s.reset_influence()
-	
-	#$Status.reset_influence()
-	#$Status2.reset_influence()
-	#$Status3.reset_influence()
-	#$Status4.reset_influence()
-	#$Status5.reset_influence()
-
