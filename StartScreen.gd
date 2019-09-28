1e466e75205bef8ea2f8c4e08ef0f47bee5a0031
enrlor <enricaloria94@gmail.com>
2019-09-28 15:04:24 +0200

    Department Managed

--- StartScreen.gd
+++ StartScreen.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+func _ready():
+	GlobalPools.load_csv()
+
 func _input(event):
     if event is InputEventKey and event.pressed:
     	get_tree().change_scene("res://SplitscreenFusion.tscn")
